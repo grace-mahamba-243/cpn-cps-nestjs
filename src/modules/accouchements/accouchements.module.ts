92ee493e05ca63417db9c7444b8515167561f3b3
Mumbere Mahamba <mumberemahambagoma@gmail.com>
2026-04-17 20:08:31 +0200

    accouchement

--- src/modules/accouchements/accouchements.module.ts
+++ src/modules/accouchements/accouchements.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AccouchementsController } from './accouchements.controller';
 import { AccouchementsService } from './accouchements.service';
+import { AccouchementEntity } from './entities/accouchement.entity';
+import { PatienteEntity } from '../patientes/entities/patiente.entity';
 
-// Ce module regroupe les composants techniques et metier de base de accouchements.
+// Ce module regroupe l entite, le service et le controleur du domaine accouchements.
 @Module({
+  imports: [TypeOrmModule.forFeature([AccouchementEntity, PatienteEntity])],
   controllers: [AccouchementsController],
   providers: [AccouchementsService],
+  exports: [AccouchementsService],
 })
 export class AccouchementsModule {}
